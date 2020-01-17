2db15b884b795bf0a7cc8f8b8ef5bfcb84ab5707
Vitali Stupin <vitali.stupin@ria.ee>
2020-01-17 13:00:23 +0200

    Fixing rights_given.py for security server version 6.22+

--- misc/rights_given.py
+++ misc/rights_given.py
@@ -28,16 +28,35 @@ conn = psycopg2.connect(
     "host={} port={} dbname={} user={} password={}".format(host, port, dbname, user, password))
 cur = conn.cursor()
 
-cur.execute("""select ar.servicecode, ar.rightsgiven,
-    ci.xroadinstance p_xroadinstance, ci.memberclass p_memberclass,
-    ci.membercode p_membercode, ci.subsystemcode p_subsystemcode,
-    si.xroadinstance c_xroadinstance, si.memberclass c_memberclass,
-    si.membercode c_membercode, si.subsystemcode c_subsystemcode,
-    si.type c_type, si.groupcode c_groupcode
-    from accessright ar
-    join client c on c.id=ar.client_id
-    join identifier ci on ci.id=c.identifier
-    join identifier si on si.id=ar.subjectid;""")
+# Old schema has accessright.servicecode column
+cur.execute("""select column_name from information_schema.columns
+    where table_name='accessright' and column_name='servicecode';""")
+
+if cur.rowcount:
+    # Work with version <=6.21
+    cur.execute("""select ar.servicecode, ar.rightsgiven,
+        ci.xroadinstance p_xroadinstance, ci.memberclass p_memberclass,
+        ci.membercode p_membercode, ci.subsystemcode p_subsystemcode,
+        si.xroadinstance c_xroadinstance, si.memberclass c_memberclass,
+        si.membercode c_membercode, si.subsystemcode c_subsystemcode,
+        si.type c_type, si.groupcode c_groupcode
+        from accessright ar
+        join client c on c.id=ar.client_id
+        join identifier ci on ci.id=c.identifier
+        join identifier si on si.id=ar.subjectid;""")
+else:
+    # Works with version >= 6.22
+    cur.execute("""select ep.servicecode, ar.rightsgiven,
+        ci.xroadinstance p_xroadinstance, ci.memberclass p_memberclass,
+        ci.membercode p_membercode, ci.subsystemcode p_subsystemcode,
+        si.xroadinstance c_xroadinstance, si.memberclass c_memberclass,
+        si.membercode c_membercode, si.subsystemcode c_subsystemcode,
+        si.type c_type, si.groupcode c_groupcode
+        from accessright ar
+        join client c on c.id=ar.client_id
+        join identifier ci on ci.id=c.identifier
+        join identifier si on si.id=ar.subjectid
+        join endpoint ep on ep.id=ar.endpoint_id;""")
 
 print('service, rightgiventime, producer, consumer, globalgroup, localgroup')
 for rec in cur:
